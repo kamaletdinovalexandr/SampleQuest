727324854bff827d4d6c9f9d0b362adb289c5d67
Aleksandr Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-03-26 22:55:15 +0300

    Добавлен базовый ScriptableObject клас - Item, его наследник DescriptiveItem, который выводит сообщение в UI при взаимодействии, конкретные SO - огонь, ледяной куб и дверь

---
+++ Assets/Scripts/IntractionItems/DescriptiveItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DescripriveItem", menuName = "items/DescriptiveItem")]
+public class DescriptiveItem : Item {
+
+    public string Descriprion;
+
+    public override void Interact() {
+        InteractionMessage.Instance.SetInteractionMessage(Descriprion);
+    }
+}
---
+++ Assets/Scripts/IntractionItems/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject {
+
+	public string Id;
+
+	public Sprite Icon;
+
+	public virtual void Interact() {}
+}
