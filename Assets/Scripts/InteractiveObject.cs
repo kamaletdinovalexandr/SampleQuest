162a0da595a7d71379d3db403ce0febc3affd418
Aleksandr Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-03-26 22:57:27 +0300

    InteractiveObject - GO хранящий в себе SO и вызывающий его реализацию метода Interact при взаимодействии с объектом

---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour {
+
+	[SerializeField] private Item _item;
+
+	public void Interact() {
+        _item.Interact();
+	}
+}
