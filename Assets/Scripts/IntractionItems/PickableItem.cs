da66268bd5872ce155478a6326625db48752c5e1
Aleksandr Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-03-27 01:07:59 +0300

    Ледяной куб - подбираемый объект в инветарный слот

---
+++ Assets/Scripts/IntractionItems/PickableItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PickableItem", menuName = "items/PickableItem")]
+public class PickableItem : Item {
+
+    public override void Interact() {
+        InventoryManager.Instance.PutItem(this);
+    }
+}
