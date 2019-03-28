e35b56979407676d6c8c7f4f71ab1da9a87122d3
Aleksandr Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-03-29 01:41:48 +0300

    Монобех для Item(Параметры Item задаются в редакторе)

---
+++ Assets/Scripts/IntractionItems/BaseItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Controllers;
+
+namespace Items {
+    public class BaseItem : MonoBehaviour {
+
+        [SerializeField] private string Name;
+        [SerializeField] private string Description;
+        [SerializeField] private ObjectType Type;
+        private Sprite _icon;
+
+        private void Awake() {
+            _icon = GetComponent<SpriteRenderer>().sprite;
+        }
+
+        public Item GetItem() {
+            return new Item(Name, Description, _icon, Type);
+        }
+    }
+}
