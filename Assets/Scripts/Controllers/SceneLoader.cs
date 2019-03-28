32089da58b57175f14149155a321667eaa23f7ec
Aleksandr Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-03-29 01:37:01 +0300

    SceneLoader - загрузчик дополнительных сцен локаций

---
+++ Assets/Scripts/Controllers/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    [SerializeField] private string StartSceneName;
+
+	void Start () {
+        SceneManager.LoadScene(StartSceneName, LoadSceneMode.Additive);
+    }
+}
