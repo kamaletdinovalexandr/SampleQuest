185a130feb783355baab4efe89a15dad21ab5768
alex <a.kamaletdinov@corpwebgames.com>
2019-05-30 09:47:50 +0300

    ScenePortal - вернул наследование от ItemView для упрощения взаимодействия

--- Assets/Scripts/IntractionItems/ScenePortal.cs
+++ Assets/Scripts/IntractionItems/ScenePortal.cs
@@ -1,24 +1,39 @@
 ﻿using UnityEngine;
 
 namespace Items {
-	public class ScenePortal : MonoBehaviour {
+	public class ScenePortal : ItemView {
 
 		private PortalState PortalState = PortalState.closed;
-		public string InputItemName;
 		public string SceneToLoad;
 
 		public bool isOpened { get { return PortalState == PortalState.open; }
 		}
-		public bool TryOpenPortal(string itemName) {
-			if (PortalState == PortalState.closed && !string.IsNullOrEmpty(itemName) && itemName == InputItemName)  {
-				PortalState = PortalState.open;
+
+		public override bool Interact(Item item, out Item craftedItem) {
+			craftedItem = new Item();
+			if (isOpened) {
+				UsePortal();
 				return true;
 			}
-		
+
+			if (TryOpenPortal(item.Name)) 
+				return true;
+
 			return false;
 		}
 
-		public void UsePortal() {
+		private bool TryOpenPortal(string itemName) {
+			if (PortalState == PortalState.closed && !string.IsNullOrEmpty(itemName) && itemName == InputItemName)  {
+				PortalState = PortalState.open;
+				var sr = GetComponent<SpriteRenderer>();
+				sr.sprite = CraftedItemIcon;
+				return true;
+			}
+
+			return false;
+		}
+
+		private void UsePortal() {
 			if (PortalState == PortalState.open) {
 				SceneLoader.Instance.ChangeLocation(SceneToLoad);
 			}
