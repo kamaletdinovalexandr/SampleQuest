d5473d7ab3c0084ddd64632454a0aac3273f12c7
alex <a.kamaletdinov@corpwebgames.com>
2019-06-04 08:50:19 +0300

    Форматирование ItemView

--- Assets/Scripts/IntractionItems/ItemView.cs
+++ Assets/Scripts/IntractionItems/ItemView.cs
@@ -5,34 +5,33 @@ using Controllers;
 
 namespace Items {
     public class ItemView : MonoBehaviour {
-
-        [Header("Item properties")]
-        public string Name;
+        [Header("Item properties")] public string Name;
         public string Description;
-		 public ItemViewType itemType;
-		 private Sprite Icon;
+        public ItemViewType itemType;
+        private Sprite Icon;
 
-        [Space]
-        [Header("Crafting area")]
-        public string InputItemName;
+        [Space] [Header("Crafting area")] public string InputItemName;
         public string CraftedItemName;
         public Sprite CraftedItemIcon;
         public string CraftedItemDeskription;
 
-		 public Item Item { get; private set; }
+        public Item Item { get; private set; }
 
-		 public Item CraftedItem { get { return Item.CraftedItem; } }
+        public Item CraftedItem {
+            get { return Item.CraftedItem; }
+        }
 
-		 public void Awake() {
+        public void Awake() {
             Icon = GetComponent<SpriteRenderer>().sprite;
-			  Item = new Item(Name, Icon, Description, InputItemName, CraftedItemName, CraftedItemIcon, CraftedItemDeskription);
-		}
+            Item = new Item(Name, Icon, Description, InputItemName, CraftedItemName, CraftedItemIcon,
+                CraftedItemDeskription);
+        }
 
-		public virtual bool Interact(Item item) {
-			if (!Item.Interact(item))
-				return false;
-			
-			return true;
+        public virtual bool Interact(Item item) {
+            if (!Item.Interact(item))
+                return false;
+
+            return true;
         }
     }
 }
