7fb98f9638d82ff292ea1b43199f0938c2909bae
alex <a.kamaletdinov@corpwebgames.com>
2019-06-07 09:54:03 +0300

    Нерабочий тест Item.Interaction

---
+++ Assets/Scripts/Tests/TestItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Items;
+
+namespace Tests {
+    public class TestItem {
+        
+        [Test]
+        public void ItemInteractionSuccess() {
+           var itemA = new Item("a", null, "itemA", "b", "", null, "");
+           var itemB = new Item("b", null, "itemB", "", "", null, "");
+           Assert.True(itemA.Interact(itemB));
+        }
+        
+        public void ItemInteractionUnSuccess() {
+            var itemA = new Item("a", null, "itemA", "b", "", null, "");
+            var itemC = new Item("c", null, "itemC", "", "", null, "");
+            Assert.True(itemA.Interact(itemC));
+        }
+    }
+}
