826dfd1b00c69a88ecb51342344f569e1eac56d1
a.kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-06-17 10:09:03 +0300

    форматирование

--- Assets/Scripts/IntractionItems/IInteractionStrategy.cs
+++ Assets/Scripts/IntractionItems/IInteractionStrategy.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
 
 namespace Items {
-    
     public interface IInteractionStrategy {
-        
         Item SlotItem { set; }
-        
         string ItemAction { get; }
         string InteractionStatus { get; }
         void Execute(GameObject go, bool endInteraction);
