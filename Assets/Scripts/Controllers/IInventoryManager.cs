1987b5484efd34c2f147dbcd74babb979bcd4ac3
alex <a.kamaletdinov@corpwebgames.com>
2019-06-21 09:47:14 +0300

    Удалены интерфейсы

--- Assets/Scripts/Controllers/IInventoryManager.cs
+++
@@ -1,9 +0,0 @@
-using Items;
-
-namespace Inventory {
-    public interface IInventoryManager {
-        bool PutItem(Item item);
-        void RemoveItem(Item item);
-        bool IsInventoryContains(Item item);
-    }
-}
