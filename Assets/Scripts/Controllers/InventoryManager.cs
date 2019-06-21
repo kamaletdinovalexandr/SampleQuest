de7fd740e0553162bc45bc9f675f368132ff0ffb
alex <a.kamaletdinov@corpwebgames.com>
2019-06-21 09:56:05 +0300

    Реализация IInventory в InventotyManager

--- Assets/Scripts/Controllers/InventoryManager.cs
+++ Assets/Scripts/Controllers/InventoryManager.cs
@@ -1,42 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Controllers;
 using Items;
 
 namespace Inventory {
-    public class InventoryManager : MonoBehaviour, IInventoryManager {
-        [SerializeField] private List<Slot> Slots;
+	public class InventoryManager : MonoBehaviour, IInventory {
+		[SerializeField] private List<Slot> Slots;
 
-        public bool PutItem(Item item) {
-            var emptySlot = Slots.FirstOrDefault(s => s.IsEmpty);
-            
-            if (emptySlot == null) {
-                return false;
-            }
+		public bool PutItem(Item item) {
+			var emptySlot = Slots.FirstOrDefault(s => s.IsEmpty);
 
-            emptySlot.AddItem(item);
-            Debug.Log("Item " + item.Name + " added");
-            return true;
-        }
+			if (emptySlot == null) {
+				return false;
+			}
 
-        public void RemoveItem(Item item) {
-            Slots.Where(s => s.Item == item).ToList().ForEach(s => {
-                s.ClearItem();
-                Debug.Log("Item " + item.Name + " removed");
-            });
-        }
+			emptySlot.AddItem(item);
+			Debug.Log("Item " + item.Name + " added");
+			return true;
+		}
 
-        public bool IsInventoryContains(Item item) {
-            return Slots.Any(s => s.Item == item);
-        }
+		public Item GetItem(Item item) {
+			Item slotItem = null;
+			var slot = Slots.FirstOrDefault(s => s.Item == item);
+			if (slot != null) {
+				slotItem = slot.Item;
+				slot.ClearItem();
+			}
+			return slotItem;
+		}
 
-        public void AddSlot(Slot newSlot) {
-            if (Slots == null)
-                Slots = new List<Slot>();
+		public void AddSlot(Slot newSlot) {
+			if (Slots == null)
+				Slots = new List<Slot>();
 
-            Slots.Add(newSlot);
-        }
-    }
+			Slots.Add(newSlot);
+		}
+	}
 }
