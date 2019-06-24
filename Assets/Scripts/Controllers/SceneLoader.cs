e522a55755ac6fb607add96ecf9f6fb16f5e2259
a.kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-06-24 10:39:57 +0300

    Сохранение состояния объектов перез изменением сцены и загрузка состояний после загрузки другой сцены

--- Assets/Scripts/Controllers/SceneLoader.cs
+++ Assets/Scripts/Controllers/SceneLoader.cs
@@ -1,20 +1,59 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using CameraExtention;
+using Items;
 
 public class SceneLoader : MBSingleton<SceneLoader> {
     [SerializeField] private string StartSceneName;
 	private string _currentScene;
+    private Dictionary<string, bool> _sceneObjects = new Dictionary<string, bool>();
+    private const string SEPARATOR = "-";
 
-	void Start () {
+    void Start () {
 		SceneManager.LoadScene(StartSceneName, LoadSceneMode.Additive);
 		_currentScene = StartSceneName;
     }
 
     public void ChangeLocation(string location) {
-		SceneManager.UnloadSceneAsync(_currentScene);
-		SceneManager.LoadScene(location, LoadSceneMode.Additive);
-		_currentScene = location;
+        SaveSceneState();
+        SceneManager.UnloadScene(_currentScene);
+        StartCoroutine(LoadScene(location));
+        _currentScene = location;
+        LoadSceneState();
+    }
+
+    private IEnumerator LoadScene(string scene) {
+        var loading = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+        while (!loading.isDone) {
+            yield return null;
+        }
+    }
+
+    private void LoadSceneState() {
+        var sceneObjects = FindObjectsOfType<ItemView>();
+        foreach (var sceneObject in sceneObjects) {
+            var objectSaveName = _currentScene + SEPARATOR + sceneObject.name;
+            if (_sceneObjects.ContainsKey(objectSaveName)) {
+                var objectState = _sceneObjects[objectSaveName];
+                sceneObject.gameObject.SetActive(objectState);
+            }
+        }
+    }
+
+    private void SaveSceneState() {
+        var sceneObjects = FindObjectsOfType<ItemView>();
+        foreach(var sceneObject in sceneObjects) {
+            var objectState = sceneObject.isActiveAndEnabled;
+            var objectSaveName = _currentScene + SEPARATOR + sceneObject.name;
+            if (!_sceneObjects.ContainsKey(objectSaveName)) {
+                _sceneObjects.Add(objectSaveName, objectState);
+            }
+            else {
+                _sceneObjects[objectSaveName] = objectState;
+            }
+        }
     }
 
     private void OnEnable() {
